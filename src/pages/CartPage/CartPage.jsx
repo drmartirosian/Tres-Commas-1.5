849876bb988ff178eac1d022f94db0afa926cf1e
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2020-01-23 11:57:26 -0600

    Cart button

--- src/pages/CartPage/CartPage.jsx
+++ src/pages/CartPage/CartPage.jsx
@@ -3,8 +3,10 @@ import React from 'react';
 const CartPage = () => {
     return (
         <div>
-            <h3>Cart</h3>
+            <h1>Cart</h1>
             <p>Cart content appears here...</p>
+            <h3>Total: 0</h3>
+            <button className="btn btn-success" >PURCHASE</button>
         </div>
     );
 };
